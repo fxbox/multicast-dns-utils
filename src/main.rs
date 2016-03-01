78cbc9093fa99966676c84c2a7d20e9373ee2d22
Aleh Zasypkin <azasypkin@mozilla.com>
2016-03-01 01:46:29 +0100

    Add CNAME alias support.

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,16 @@ use multicast_dns::host::HostManager;
 use multicast_dns::discovery::discovery_manager::*;
 
 docopt!(Args derive Debug, "
-Usage: multicast-dns-utils [-t <type>] [-n <hostname>]
+Usage: multicast-dns-utils [-t <type>] [-n <hostname>] [-a <alias>]
 
 Options:
     -t, --type <type>       Look for service of the specified type (e.g. _device-info._tcp).
-    -n, --name <hostname>   Set custom host hanme.
+    -n, --name <hostname>   Set custom host name.
+    -a, --alias <alias>     Set custom host name alias.
 ",
         flag_type: Option<String>,
-        flag_name: Option<String>);
+        flag_name: Option<String>,
+        flag_alias: Option<String>);
 
 fn main() {
     let args: Args = Args::docopt().decode().unwrap_or_else(|e| e.exit());
@@ -25,26 +27,39 @@ fn main() {
         let discovery_manager = DiscoveryManager::new();
 
         let on_service_resolved = |service: ServiceInfo| {
-            println!("Service resolved: {:?}", service);
+            println!("=   {}   {:?}   {}   {}   {}",
+                     service.interface,
+                     service.protocol,
+                     service.name.as_ref().unwrap(),
+                     service.type_name.as_ref().unwrap(),
+                     service.domain.as_ref().unwrap());
+            println!("    hostname = [{}]", service.host_name.as_ref().unwrap());
+            println!("    address = [{}]", service.address.as_ref().unwrap());
+            println!("    port = [{}]", service.port);
+            println!("    txt = [{}]", service.txt.as_ref().unwrap());
         };
 
         let on_service_discovered = |service: ServiceInfo| {
-            println!("Service discovered: {:?}", service);
+            println!("+   {}   {:?}   {}   {}   {}",
+                     service.interface,
+                     service.protocol,
+                     service.name.as_ref().unwrap(),
+                     service.type_name.as_ref().unwrap(),
+                     service.domain.as_ref().unwrap());
 
-            let resolve_listeners = ResolveListeners {
-                on_service_resolved: Some(&on_service_resolved),
-            };
-
-            discovery_manager.resolve_service(service, resolve_listeners);
+            discovery_manager.resolve_service(service,
+                                              ResolveListeners {
+                                                  on_service_resolved: Some(&on_service_resolved),
+                                              });
         };
 
-        let on_all_discovered = || {
+        let on_all_services_discovered = || {
             println!("All services has been discovered");
         };
 
         let discovery_listeners = DiscoveryListeners {
             on_service_discovered: Some(&on_service_discovered),
-            on_all_discovered: Some(&on_all_discovered),
+            on_all_discovered: Some(&on_all_services_discovered),
         };
 
         discovery_manager.discover_services(&args.flag_type.unwrap(), discovery_listeners);
@@ -58,5 +73,19 @@ fn main() {
                  host_manager.get_name(),
                  &new_host_name);
         println!("New Host name: {:?}", host_manager.set_name(&new_host_name));
+    }
+
+    if args.flag_alias.is_some() {
+        let host_manager = HostManager::new();
+        let new_alias = args.flag_alias.unwrap();
+
+        println!("New alias ({}) is requested", &new_alias);
+
+        host_manager.add_name_alias(&new_alias);
+
+        println!("New alias \"{}\" is active until program is terminated.",
+                 new_alias);
+
+        loop {}
     }
 }
